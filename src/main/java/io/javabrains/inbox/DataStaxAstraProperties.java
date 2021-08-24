2f3519dded48fd8264880cf457ec0e49386f9adc
Koushik Kothagal <koush.s@gmail.com>
2021-08-24 13:20:47 -0700

    Initial Setup

---
+++ src/main/java/io/javabrains/inbox/DataStaxAstraProperties.java
@@ -0,0 +1,20 @@
+package io.javabrains.inbox;
+
+import java.io.File;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "datastax.astra")
+public class DataStaxAstraProperties {
+
+    private File secureConnectBundle;
+
+    public File getSecureConnectBundle() {
+        return secureConnectBundle;
+    }
+
+    public void setSecureConnectBundle(File secureConnectBundle) {
+        this.secureConnectBundle = secureConnectBundle;
+    }
+}
---
+++ src/main/java/io/javabrains/inbox/SpringGitHubLoginApplication.java
@@ -0,0 +1,35 @@
+package io.javabrains.inbox;
+
+import java.nio.file.Path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringGitHubLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitHubLoginApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public String user(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return principal.getAttribute("name");
+	}
+
+	@Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
+	
+
+}
